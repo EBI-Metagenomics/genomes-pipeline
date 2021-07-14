0b42dc6fd4212e055f34ca3646c726e340fa3f63
Ekaterina Sakharova <kates@ebi.ac.uk>
2021-07-14 13:50:00 +0100

    add yml for ips

--- download_db.sh
+++ download_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # download db for GUNC
-wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/gunc_db_2.0.4.dmnd.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/gunc_db_2.0.4.dmnd.gz && gunzip gunc_db_2.0.4.dmnd.gz
 
 # eggnog dbs
-wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/eggnog_db.tgz
+wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/eggnog_db.tgz && tar -xvzf eggnog_db.tgz
 
 # ips dbs
-wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/interproscan_5.52-86.0.data.tgz
+wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/interproscan_5.52-86.0.data.tgz && tar -xvzf interproscan_5.52-86.0.data.tgz
